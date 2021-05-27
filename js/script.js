71c6aa188361bba0268157b27a55694ca729b88f
Giulia <giuliatullia93@gmail.com>
2021-05-27 17:38:28 +0200

    add-stampa-video

--- js/script.js
+++ js/script.js
@@ -5,22 +5,28 @@ var età = prompt ('Scrivi la tua età')
 var prezzo = km * 0.21
 console.log('totale senza sconto',prezzo)
 
-var prezzoMinorenni = prezzo * 20 / 100
+var prezzoMinorenni = prezzo - prezzo * 20 / 100
 console.log('prezzo scontato minorenni', prezzoMinorenni)
 
-var prezzoOver65 = prezzo * 40 / 100
+var prezzoOver65 = prezzo -prezzo * 40 / 100
 console.log('prezzo scontato over 65', prezzoOver65)
 
-
+var outcome = prezzo
 
 if (età < 18) {
-    outcome = prezzoMinorenni
+    var outcome = prezzoMinorenni
     console.log('prezzo scontato minorenni');
 } else if (età > 65) { 
-    outcome = prezzoOver65
+    var outcome = prezzoOver65
     console.log('prezzo scontato over 65');
 }
   
+
+
+
+document.write('Totale biglietto:',outcome);
+
+
 
 
    
